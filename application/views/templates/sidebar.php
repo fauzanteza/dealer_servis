6f35bbea7a3bce0faf257b9a2adf3aa01b39e506
Fauzan <fauzantezasaputra@gmail.com>
2026-01-05 21:23:47 +0700

    sipp3

--- application/views/templates/sidebar.php
+++ application/views/templates/sidebar.php
@@ -2,10 +2,10 @@
             <h5 class="text-center mb-4">Menu Utama</h5>
             <a href="<?= base_url('index.php/dealer') ?>">📊 Dashboard</a>
             <hr>
-            <small class="text-muted ms-3">BENGKEL</small>
+             <class="text-muted ms-3">BENGKEL</>
             <a href="<?= base_url('index.php/dealer/servis') ?>">🔧 Antrian Servis</a>
             <br>
-            <small class="text-muted ms-3">PENJUALAN</small>
+            <class="text-muted ms-3">PENJUALAN</class=>
             <a href="<?= base_url('index.php/dealer/motor') ?>">🏍️ Data Stok Motor</a>
             <a href="<?= base_url('index.php/dealer/penjualan') ?>">💰 Laporan Penjualan</a>
             <hr>
